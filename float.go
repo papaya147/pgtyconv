aad18e7f0bc511c92ffa3b76e0d246f570df4e17
Abhinav Srivatsa <asrivatsa6@gmail.com>
2024-06-25 19:17:30 +0530

    add float64 conv

--- float.go
+++ float.go
@@ -30,3 +30,32 @@ func ToFloat32Ptr(t pgtype.Float4) *float32 {
 	p := ToFloat32(t)
 	return &p
 }
+
+// FromFloat64 converts a float64 to a pgtype.Float8.
+// An empty input float64 (0) will output a null pgtype.Float8.
+func FromFloat64(f float64) pgtype.Float8 {
+	return pgtype.Float8{Float64: f, Valid: !isZero(f)}
+}
+
+// FromFloat64Ptr converts a *float64 to a pgtype.Float8.
+// A nil input will output a null pgtype.Float8, otherwise will call FromFloat64.
+func FromFloat64Ptr(f *float64) pgtype.Float8 {
+	if f == nil {
+		return pgtype.Float8{}
+	}
+	return FromFloat64(*f)
+}
+
+// ToFloat64 converts a pgtype.Float8 to a float64.
+func ToFloat64(t pgtype.Float8) float64 {
+	return ternary(t.Valid, t.Float64, zero[float64]())
+}
+
+// ToFloat64Ptr converts a pgtype.Float8 to a *float64.
+func ToFloat64Ptr(t pgtype.Float8) *float64 {
+	if !t.Valid {
+		return nil
+	}
+	p := ToFloat64(t)
+	return &p
+}
--- float_test.go
+++ float_test.go
@@ -60,3 +60,57 @@ func TestToFloat32Ptr(t *testing.T) {
 	i = ToFloat32Ptr(pgi)
 	require.Nil(t, i)
 }
+
+func TestFromFloat64(t *testing.T) {
+	i, err := randomize.Do[float64]()
+	require.NoError(t, err)
+	pgi := FromFloat64(i)
+	require.Equal(t, i, pgi.Float64)
+	require.Equal(t, true, pgi.Valid)
+
+	i = 0
+	pgi = FromFloat64(i)
+	require.Equal(t, i, pgi.Float64)
+	require.Equal(t, false, pgi.Valid)
+}
+
+func TestFromFloat64Ptr(t *testing.T) {
+	i, err := randomize.Do[float64]()
+	require.NoError(t, err)
+	pgi := FromFloat64Ptr(&i)
+	require.Equal(t, i, pgi.Float64)
+	require.Equal(t, true, pgi.Valid)
+
+	i = 0
+	pgi = FromFloat64Ptr(&i)
+	require.Equal(t, i, pgi.Float64)
+	require.Equal(t, false, pgi.Valid)
+
+	pgi = FromFloat64Ptr(nil)
+	require.Equal(t, float64(0), pgi.Float64)
+	require.Equal(t, false, pgi.Valid)
+}
+
+func TestToFloat64(t *testing.T) {
+	pgi, err := randomize.Do[pgtype.Float8]()
+	require.NoError(t, err)
+	pgi.Valid = true
+	i := ToFloat64(pgi)
+	require.Equal(t, i, pgi.Float64)
+
+	pgi.Valid = false
+	i = ToFloat64(pgi)
+	require.Equal(t, float64(0), i)
+}
+
+func TestToFloat64Ptr(t *testing.T) {
+	pgi, err := randomize.Do[pgtype.Float8]()
+	require.NoError(t, err)
+	pgi.Valid = true
+	i := ToFloat64Ptr(pgi)
+	require.Equal(t, *i, pgi.Float64)
+
+	pgi.Valid = false
+	i = ToFloat64Ptr(pgi)
+	require.Nil(t, i)
+}
